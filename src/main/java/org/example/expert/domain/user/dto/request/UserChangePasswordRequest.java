6fcb168b701e77371de0710215adf7cafc936b5c
tgg <qazplmtfgcv123@gmail.com>
2025-02-23 18:45:46 +0900

    1-3 : Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,9 @@
 package org.example.expert.domain.user.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +16,7 @@ public class UserChangePasswordRequest {
     @NotBlank
     private String oldPassword;
     @NotBlank
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(regexp = "^(.*\\d.*[A-Z].*|.*[A-Z].*\\d.*)$", message = "비밀번호는 숫자와 대문자를 포함해야 됩니다.")
     private String newPassword;
 }
